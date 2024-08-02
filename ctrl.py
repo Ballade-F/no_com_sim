d5b5386e7bca87701374c27939490d5b1ddf88fb
Ballade-F <258300018@qq.com>
2024-08-02 16:58:45 +0800

    finish kalman

--- ctrl.py
+++ ctrl.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 #state space model
 #state: [x, y, yaw, v, w]
 class State():
-    def __init__(self, x0:float=0, y0:float=0, yaw0:float=0, v0:float=0, w0:float=0, dt:float=0.1):
+    def __init__(self, x0:float=0, y0:float=0, yaw0:float=0, v0:float=0, w0:float=0, dt:float=0.1,
+                  noise_std_v:float=0, noise_std_w:float=0, noise_std_x:float=0, noise_std_y:float=0):
         self.X = np.array([x0, y0, yaw0, v0, w0])
         self.X_1 = np.array([x0, y0, yaw0, v0, w0])
 
@@ -12,44 +13,96 @@ class State():
 
         self.n_states = 5
         self.n_observation = 2
+        self.n_noise_pred = 2
+
+        self.noise_std_v = noise_std_v
+        self.noise_std_w = noise_std_w
+        self.noise_std_x = noise_std_x
+        self.noise_std_y = noise_std_y
+
         
-    def update(self, v_new:float, w_new:float):
+
+    #with noise
+    def forward(self):
+        v_pred = self.X[3] + np.random.normal(0, self.noise_std_v)
+        w_pred = self.X[4] + np.random.normal(0, self.noise_std_w)
         self.X_1 = self.X
-        self.X = np.array([self.X[0] + self.X[3] * np.cos(self.X[2]) * self.dt,
-                           self.X[1] + self.X[3] * np.sin(self.X[2]) * self.dt,
-                           self.X[2] + self.X[4] * self.dt,
-                           v_new,
-                           w_new])
+        self.X = np.array([self.X_1[0] + v_pred*np.cos(self.X_1[2])*self.dt,
+                            self.X_1[1] + v_pred*np.sin(self.X_1[2])*self.dt,
+                            self.X_1[2] + w_pred*self.dt,
+                            self.X_1[3],
+                            self.X_1[4]])
+
+    #without noise  
+    def predict(self):
+        self.X_1 = self.X
+        self.X = np.array([self.X_1[0] + self.X_1[3]*np.cos(self.X_1[2])*self.dt,
+                            self.X_1[1] + self.X_1[3]*np.sin(self.X_1[2])*self.dt,
+                            self.X_1[2] + self.X_1[4]*self.dt,
+                            self.X_1[3],
+                            self.X_1[4]])
+        
+    def update_vw(self, v:float, w:float):
+        self.X[3] = v
+        self.X[4] = w
+                           
 
     def observation(self):
-        return self.X[:self.n_observation]
+        x_obs = self.X[0] + np.random.normal(0, self.noise_std_x)
+        y_obs = self.X[1] + np.random.normal(0, self.noise_std_y)
+        return np.array([x_obs, y_obs])
     
 class ExtendKalman():
-    def __init__(self, state:State):
+    def __init__(self, state:State,v_max:float=float('inf'), w_max:float=float('inf')):
         self.state = state
+        self.Q = np.diag([state.noise_std_v**2, state.noise_std_w**2])
+        self.R = np.diag([state.noise_std_x**2, state.noise_std_y**2])
+
+        self.A = np.eye(state.n_states)
+        self.W = np.zeros((state.n_states, state.n_noise_pred))
         
         self.P = np.eye(state.n_states)
-        self.Q = np.eye(state.n_states)
-        self.R = np.eye(state.n_observation)
         self.H = np.zeros((state.n_observation, state.n_states))
-        self.I = np.eye(state.n_states)
-        self.F = np.zeros((state.n_states, state.n_states))
+        self.H[0, 0] = 1
+        self.H[1, 1] = 1
         self.K = np.zeros((state.n_states, state.n_observation))
-    
+
+        self.v_max = v_max
+        self.w_max = w_max
+
     def predict(self):
-        self.F = np.array([[1, 0, -self.state.X_1[3] * np.sin(self.state.X_1[2]) * self.state.dt, np.cos(self.state.X_1[2]) * self.state.dt, 0],
-                           [0, 1, self.state.X_1[3] * np.cos(self.state.X_1[2]) * self.state.dt, np.sin(self.state.X_1[2]) * self.state.dt, 0],
-                           [0, 0, 1, 0, self.state.dt],
-                           [0, 0, 0, 1, 0],
-                           [0, 0, 0, 0, 1]])
-        self.P = self.F @ self.P @ self.F.T + self.Q
-    
-    def update(self, z:np.ndarray):
-        self.H = np.array([[1, 0, 0, 0, 0],
-                           [0, 1, 0, 0, 0]])
-        self.K = self.P @ self.H.T @ np.linalg.inv(self.H @ self.P @ self.H.T + self.R)
-        self.state.X = self.state.X + self.K @ (z - self.H @ self.state.X)
-        self.P = (self.I - self.K @ self.H) @ self.P
+        self.A[0, 2] = -self.state.X[3]*np.sin(self.state.X[2])*self.state.dt
+        self.A[0, 3] = np.cos(self.state.X[2])*self.state.dt
+        self.A[1, 2] = self.state.X[3]*np.cos(self.state.X[2])*self.state.dt
+        self.A[1, 3] = np.sin(self.state.X[2])*self.state.dt
+        self.A[2, 4] = self.state.dt
+
+        self.W[0, 0] = 0.5*self.state.dt**2*np.cos(self.state.X[2])
+        self.W[1, 0] = 0.5*self.state.dt**2*np.sin(self.state.X[2])
+        self.W[2, 1] = 0.5*self.state.dt**2
+        self.W[3, 0] = self.state.dt
+        self.W[4, 1] = self.state.dt
+
+        self.P = np.dot(self.A, self.P).dot(self.A.T) + np.dot(self.W, self.Q).dot(self.W.T)
+
+        self.state.predict()
+
+        return self.state.X
+
+    #z: observation [x_obs, y_obs]
+    #Posterior estimation of state
+    def update(self, z:np.array):
+        self.K = np.dot(self.P, self.H.T).dot(np.linalg.inv(np.dot(self.H, self.P).dot(self.H.T) + self.R))
+        self.state.X = self.state.X + np.dot(self.K, z - self.state.observation())
+        self.P = np.dot(np.eye(self.state.n_states) - np.dot(self.K, self.H), self.P)
+
+        #limit v, w estimation
+        self.state.X[3] = min(self.v_max, max(0, self.state.X[3]))
+        self.state.X[4] = min(self.w_max, max(-self.w_max, self.state.X[4]))
+        
+        return self.state.X
 
 
-    
+    #x: [x, y, yaw], u: [v, w]
+    class MPCCtrl():
+        
