bb536248ae0f74f937d693ae7c47f6c271a287ec
Ballade-F <258300018@qq.com>
2024-08-12 15:02:55 +0800

    to debug mpc

--- test.py
+++ test.py
@@ -1,47 +1,120 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import heapq
+import ctrl
 
-class astar_node():
-    def __init__(self, x:int, y:int):
-        self.x = x
-        self.y = y
-        self.g = float('inf')
-        self.h = 0
-        self.f = float('inf')
-        self.parent = None
-        # flag for open list and close list
-        self.set_flag = 0
+if __name__ == '__main__':
+    #generate random path
+    v = 0.4
+    w = 0.2
+    dt = 0.1
+    time = 30
+    n = int(time/dt)
+    x = np.zeros(n)
+    y = np.zeros(n)
+    yaw = np.zeros(n)
+    car_state = ctrl.State(x0=0, y0=0, yaw0=0, v0=v, w0=w, dt=dt)
 
-    def reset(self):
-        self.g = float('inf')
-        self.h = 0
-        self.f = float('inf')
-        self.parent = None
-        self.set_flag = 0
+    for i in range(n):
+        x[i] = car_state.X[0]
+        y[i] = car_state.X[1]
+        yaw[i] = car_state.X[2]
+        car_state.forward()
+        if i == 100:
+            car_state.update_vw(0.4, -0.15)
+        if i == 200:
+            car_state.update_vw(0.4, 0.25)
 
-if __name__ == "__main__":
-    # #test =
-    # node0 = astar_node(1, 2)
-    # node_test = node0
-    # print(node0.g)
-    # node_test.g = 1
-    # print(node0.g)
-    # a = 1
-    # b = a
-    # b = 5
-    # print(a)
+    # plt.plot(x, y)
+    # plt.show()
 
-    #test heapq
-    heap = []
-    node_list = [astar_node(1, 2), astar_node(2, 3), astar_node(3, 4)]
-    node_list[0].f = 1
-    node_list[1].f = 2
-    node_list[2].f = 3
-    for node in node_list:
-        heapq.heappush(heap, (node.f, node))
-    print(heap)
-    node_list[0].f = 4
-    print(heap)
-    heapq.heappush(heap, (node_list[0].f, node_list[0]))
-    print(heap)
+    #test EKF
+    noise_std_v = 0.1
+    noise_std_w = 0.1
+    noise_std_x = 0.05
+    noise_std_y = 0.05
+    car_state_noise = ctrl.State(x0=0, y0=0, yaw0=0, v0=v, w0=w, dt=dt,
+                                 noise_std_v=noise_std_v, noise_std_w=noise_std_w,
+                                 noise_std_x=noise_std_x, noise_std_y=noise_std_y)
+    x_noise = np.zeros(n)
+    y_noise = np.zeros(n)
+    x_noise[0] = car_state_noise.X[0]
+    y_noise[0] = car_state_noise.X[1]
+    car_state_ekf = ctrl.State(x0=0, y0=0, yaw0=0, v0=0, w0=0, dt=dt,
+                           noise_std_v=noise_std_v, noise_std_w=noise_std_w,
+                           noise_std_x=noise_std_x, noise_std_y=noise_std_y)
+    ekf = ctrl.ExtendKalman(car_state_ekf)
+    x_ekf = np.zeros(n)
+    y_ekf = np.zeros(n)
+    x_ekf[0] = car_state_ekf.X[0]
+    y_ekf[0] = car_state_ekf.X[1]
+    x_observation = np.zeros(n)
+    y_observation = np.zeros(n)
+    x_observation[0] = car_state_noise.X[0]
+    y_observation[0] = car_state_noise.X[1]
+    for i in range(1, n):
+        x_noise[i] = car_state_noise.X[0]
+        y_noise[i] = car_state_noise.X[1]
+        car_state_noise.forward()
+        z = car_state_noise.observation()
+        ekf.predict()
+        ekf.update(z)
+        x_observation[i] = z[0]
+        y_observation[i] = z[1]
+        x_ekf[i] = car_state_ekf.X[0]
+        y_ekf[i] = car_state_ekf.X[1]
+
+        if i == 100:
+            car_state_noise.update_vw(0.4, -0.15)
+        if i == 200:
+            car_state_noise.update_vw(0.4, 0.25)
+    
+    # plt.plot(x_noise, y_noise, label='noisy')
+    # plt.plot(x_ekf, y_ekf, label='ekf')
+    # plt.plot(x_observation, y_observation, label='observation')
+    # plt.legend()
+    # plt.show()
+
+    #test MPC
+    vehicle = ctrl.CarModel()
+    Q = np.matrix(np.eye(vehicle.n_x) * 2)
+    R = np.matrix(np.eye(vehicle.n_u) * 1.5)
+    Qf = np.matrix(np.eye(vehicle.n_x) * 2)
+    A_hat, B_hat = vehicle.stateSpaceModel(vehicle.state, vehicle.u)
+    N_mpc = 5
+    mpc = ctrl.MPCCtrl(A_hat, B_hat, Q, R, Qf, N = N_mpc)
+    #debug
+    print('Q: ', Q, 'R: ', R, 'Qf: ', Qf)
+    x_mpc = np.zeros(n)
+    y_mpc = np.zeros(n)
+
+    track_ref = np.zeros((n, vehicle.n_x))
+    track_ref[:, 0] = x
+    track_ref[:, 1] = y
+    track_ref[:, 2] = yaw
+    state_ref = np.zeros((N_mpc, vehicle.n_x))
+    state_ref_0 = np.zeros(vehicle.n_x)
+    
+    for i in range(n):
+        x_mpc[i] = vehicle.x
+        y_mpc[i] = vehicle.y
+        #debug
+        # print('state: ', vehicle.state)
+        # print('u: ', vehicle.u)
+        A_hat, B_hat = vehicle.stateSpaceModel(vehicle.state, vehicle.u)
+        idx = mpc.calc_track_idx(vehicle.x, vehicle.y, track_ref)
+        state_error0 = vehicle.state - track_ref[idx].reshape(-1, 1)
+        state_ref = mpc.calc_ref_trajectory(track_ref, idx, N_mpc)
+        
+        u = mpc.solve(state_error0,state_ref,A_hat,B_hat,Q,R,Qf,N_mpc)[0:vehicle.n_u]
+        #debug
+        print('i:', i,'u: ', u[0], u[1])
+        vehicle.update(u[0], u[1])
+
+        
+
+
+
+    plt.plot(x, y, label='real')
+    plt.plot(x_mpc, y_mpc, label='mpc')
+    plt.legend()
+    plt.show()
---
+++ test_py.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+#test reshape
+a = np.array([[1, 2], [3, 4]])
+print(a)
+print(a.reshape(-1, 1))
