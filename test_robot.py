12ef1aa06c41440466b8231203659060018927e9
Ballade_F <258300018@qq.com>
2024-11-03 10:23:52 +0800

    在写测试

--- test_robot.py
+++ test_robot.py
@@ -14,3 +14,23 @@ import task_allocation.ga_allocation_lib as ga
 import net.allocation as allocation
 import net.intention as intention
 from robot import Robot
+
+
+
+def _state_update(state,u,dt):
+    state[0] += u[0]*np.cos(state[2])*dt
+    state[1] += u[0]*np.sin(state[2])*dt
+    state[2] += u[1]*dt
+    state[3] = u[0]
+    state[4] = u[1]
+    return state
+
+def _judge_arrival(state, target, r):
+    if np.linalg.norm(state[:2]-target[:2]) < r:
+        print('arrived')
+        return True
+    else:
+        return False
+    
+def test_robot_2():
+    pass
